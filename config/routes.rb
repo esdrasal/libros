c97a0cf0c7fdea0c1b00aaafa1be43161ec886c4
esdras <esdras@talkabroad.com>
2025-08-15 17:38:13 -0600

    testing FTP

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index" :D
+  # root "posts#index"
   root "libros#index"
   resources :libros do
     member do
